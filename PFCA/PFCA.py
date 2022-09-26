debda9df137d22219ab0bfb1df89cd30c22a119d
LinkAnJarad <79294502+LinkAnJarad@users.noreply.github.com>
2022-09-26 15:09:34 +0800

    Updated docstring

--- PFCA/PFCA.py
+++ PFCA/PFCA.py
@@ -4,11 +4,17 @@ from torch import nn
 class ParameterFreeChannelAttention(nn.Module):
     '''
     Shi, Yuxuan & Xu, Jun & Yang, Lingxiao & An, Wangpeng & Zhen, Xiantong. (2022). PARAMETER-FREE CHANNEL ATTENTION FOR IMAGE CLASSIFICATION AND SUPER-RESOLUTION. 10.13140/RG.2.2.20039.78241.
+    
+    Computes attention for each input feature map without the use of paramters.
+    
+    Parameters:
+    feature_map_size (int): Length/width of the input feature map
+    _lambda: A hyperparameter that is added to the variance (default: 1e-4)
     '''
-    def __init__(self, kernel_size, _lambda=1e-4):
+    def __init__(self, feature_map_size, _lambda=1e-4):
         super().__init__()
         
-        self.pooling = nn.AvgPool2d(kernel_size)
+        self.pooling = nn.AvgPool2d(feature_map_size)
         self._lambda = _lambda
         
     def forward(self, x):
